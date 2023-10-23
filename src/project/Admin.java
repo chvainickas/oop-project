8feb166810843f0238b2e5440ebd1b69dbee9d05
Edward Chvainickas <chvainickas@gmail.com>
2023-10-23 08:08:12 +0100

    Changed the classes in Login section to make more sense

--- src/project/Admin.java
+++ src/project/Admin.java
@@ -1,5 +1,11 @@
 package project;
 
 public class Admin extends User {
-    
+    int adminID;
+
+    public Admin(String email, String userName, String userPass, int adminID) {
+        super(email, userName, userPass);
+        this.adminID = adminID;
+    }
+
 }
--- src/project/AppUser.java
+++ src/project/AppUser.java
@@ -1,5 +1,20 @@
 package project;
 
 public class AppUser extends User {
+    int userID;
+    String firstName;
+    String lastName;
+    int age;
+    boolean termsAndConditions;
+
+    public AppUser(String email, String userName, String userPass, int userID, String firstName, String lastName,
+            int age, boolean termsAndConditions) {
+        super(email, userName, userPass);
+        this.userID = userID;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.age = age;
+        this.termsAndConditions = termsAndConditions;
+    }
 
 }
