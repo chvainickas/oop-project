e49e1b5cc72a17f116dcc27abb6d28d76dce089b
chvainickas <chvainickas@gmail.com>
2023-12-11 17:49:18 +0000

    changed the admin file

--- src/project/Admin.java
+++ src/project/Admin.java
@@ -4,16 +4,8 @@ public class Admin extends User {
     int adminID;
 
     public Admin(String email, String userName, String userPass, String role, int adminID) {
-        super(email, userName, userPass, role);
-        this.adminID = adminID;
+        super(email, userName, userPass, role, adminID, role, role, adminID);
     }
 
-    public int getAdminID() {
-        return adminID;
-    }
-
-    public void setAdminID(int adminID) {
-        this.adminID = adminID;
-    }
 
 }
