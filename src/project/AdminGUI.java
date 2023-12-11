6ef7282050b70a67a606d710012639b8b05b4b6f
chvainickas <chvainickas@gmail.com>
2023-12-11 16:42:37 +0000

    revert merge

--- src/project/AdminGUI.java
+++ src/project/AdminGUI.java
@@ -8,12 +8,12 @@ package project;
  *
  * @author edward
  */
-public class AdminForm extends javax.swing.JFrame {
+public class AdminGUI extends javax.swing.JFrame {
 
 	/**
 	 * Creates new form AdminForm
 	 */
-	public AdminForm() {
+	public AdminGUI() {
 		initComponents();
 	}
 
@@ -107,22 +107,19 @@ public class AdminForm extends javax.swing.JFrame {
 				}
 			}
 		} catch (ClassNotFoundException ex) {
-			java.util.logging.Logger.getLogger(AdminForm.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+			java.util.logging.Logger.getLogger(AdminGUIclass.getName()).log(java.util.logging.Level.SEVERE, null, ex);
 		} catch (InstantiationException ex) {
-			java.util.logging.Logger.getLogger(AdminForm.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+			java.util.logging.Logger.getLogger(AdmAdminGUIass.getName()).log(java.util.logging.Level.SEVERE, null, ex);
 		} catch (IllegalAccessException ex) {
-			java.util.logging.Logger.getLogger(AdminForm.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+			java.util.logging.Logger.getLogger(AdmiAdminGUIss.getName()).log(java.util.logging.Level.SEVERE, null, ex);
 		} catch (javax.swing.UnsupportedLookAndFeelException ex) {
-			java.util.logging.Logger.getLogger(AdminForm.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+			java.util.logging.Logger.getLogger(AdminAdminGUIs.getName()).log(java.util.logging.Level.SEVERE, null, ex);
 		}
 		//</editor-fold>
 
-		/* Create and display the form */
-		java.awt.EventQueue.invokeLater(new Runnable() {
-			public void run() {
-				new AdminForm().setVisible(true);
-			}
-		});
+		/* 		//</editor-fold>
+Create and display the form */
+		
 	}
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
