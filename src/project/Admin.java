0f69c8b49640fb96582527be0368455faa978e61
chvainickas <chvainickas@gmail.com>
2023-12-11 18:10:33 +0000

    manually merged the project

--- src/project/Admin.java
+++ src/project/Admin.java
@@ -4,8 +4,16 @@ public class Admin extends User {
     int adminID;
 
     public Admin(String email, String userName, String userPass, String role, int adminID) {
-        super(email, userName, userPass, role, adminID, role, role, adminID);
+        super(email, userName, userPass, role);
+        this.adminID = adminID;
     }
 
+    public int getAdminID() {
+        return adminID;
+    }
+
+    public void setAdminID(int adminID) {
+        this.adminID = adminID;
+    }
 
 }
