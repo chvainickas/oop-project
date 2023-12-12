27f6dfb7f000fd3c19db20fb90dac4679ca8fe7c
chvainickas <38086543+chvainickas@users.noreply.github.com>
2023-12-12 16:15:36 +0000

    created relative path for csv for login and registration

--- src/project/User.java
+++ src/project/User.java
@@ -15,7 +15,8 @@ class User {
     int age;
     User[] users;
 
-    public User(String email, String userName, String userPass, String role, int userID, String firstName, String lastName, int age) {
+    public User(String email, String userName, String userPass, String role, int userID, String firstName,
+            String lastName, int age) {
         this.email = email;
         this.userName = userName;
         this.userPass = userPass;
@@ -57,6 +58,7 @@ class User {
     public void setRole(String role) {
         this.role = role;
     }
+
     public int getUserID() {
         return userID;
     }
@@ -88,7 +90,7 @@ class User {
     public void setAge(int age) {
         this.age = age;
     }
-    
+
     public User[] getUsers() {
         return users;
     }
@@ -97,17 +99,23 @@ class User {
         this.users = users;
     }
 
-
-    
     public static boolean login(String inputUsername, String inputPassword) {
-        String filePath = "/home/edward/workspace/oop-project/src/project/users.csv"; // directory to file path
+        String filePath = "src/project/users.csv"; // directory to file path
 
         try (BufferedReader csvReader = new BufferedReader(new FileReader(filePath))) {
             String line;
             while ((line = csvReader.readLine()) != null) {
-                String[] fields = line.split(",");  // splits the csv into fields
-                if (fields.length >= 3 && fields[1].equals(inputUsername) && fields[2].equals(inputPassword)) { // checks the logins from based off the csv file
-                    return true; 
+                String[] fields = line.split(","); // splits the csv into fields
+                if (fields.length >= 3 && fields[1].equals(inputUsername) && fields[2].equals(inputPassword)) { // checks
+                                                                                                                // the
+                                                                                                                // logins
+                                                                                                                // from
+                                                                                                                // based
+                                                                                                                // off
+                                                                                                                // the
+                                                                                                                // csv
+                                                                                                                // file
+                    return true;
                 }
             }
         } catch (IOException e) {
